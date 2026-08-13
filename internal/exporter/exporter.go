4c86ff9803414b6caadff60f83b215ee042fa1a4
Harald Koch <harald.koch@gmail.com>
2026-08-13 15:31:18 -0400

    chore: reorganize code into cmd and internal folders

--- internal/exporter/exporter.go
+++ internal/exporter/exporter.go
@@ -1,7 +1,7 @@
 package exporter
 
 import (
-	"github.com/haraldkoch/tplink-plug-exporter/kasa"
+	"github.com/haraldkoch/tplink-plug-exporter/internal/kasa"
 	"github.com/prometheus/client_golang/prometheus"
 	log "github.com/sirupsen/logrus"
 )
