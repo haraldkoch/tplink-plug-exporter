007e0f214d8dfa314df382c1478fe3e0225fa286
Harald Koch <chk@pobox.com>
2026-08-14 11:48:46 -0400

    fix: make goreleaser and PR workflows work correctly (#77)
    * chore: remove matrix
    
    * chore: remove darwin, windows
    
    * chore: mise tools
    
    * remove cosign
    
    * chore: add snapshot
    
    * go 1.26.6
    
    * chore: remove test step that we removed
    
    * chore: lint fixes

--- internal/exporter/server.go
+++ internal/exporter/server.go
@@ -25,12 +25,12 @@ func (s *HttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s.mux.ServeHTTP(w, r)
 }
 
-//https://github.com/oliver006/redis_exporter/blob/master/exporter.go
+// https://github.com/oliver006/redis_exporter/blob/master/exporter.go
 func (s *HttpServer) ScrapeHandler(w http.ResponseWriter, r *http.Request) {
 	target := r.URL.Query().Get("target")
 	if target == "" {
 		http.Error(w, "'target' parameter must be specified", 400)
-		//e.targetScrapeRequestErrors.Inc()
+		// e.targetScrapeRequestErrors.Inc()
 		return
 	}
 
